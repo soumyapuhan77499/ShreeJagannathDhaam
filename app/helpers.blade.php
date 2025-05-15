78c3c1159e2353a419ce6dc855af4b34eab76018
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-15 13:16:03 +0530

    d

--- app/helpers.blade.php
+++ app/helpers.blade.php
@@ -1,14 +1,12 @@
-@php
-function convertToOdiaTime($time)
-{
-    // Convert digits
-    $odiaDigits = ['0'=>'୦','1'=>'୧','2'=>'୨','3'=>'୩','4'=>'୪','5'=>'୫','6'=>'୬','7'=>'୭','8'=>'୮','9'=>'୯'];
-    $converted = strtr($time, $odiaDigits);
+<?php
 
-    // Convert AM/PM
-    $converted = str_replace('AM', 'ଏ.ଏମ.', $converted);
-    $converted = str_replace('PM', 'ପି.ଏମ.', $converted);
-
-    return $converted;
+if (!function_exists('convertToOdiaTime')) {
+    function convertToOdiaTime($time)
+    {
+        $odiaDigits = ['0'=>'୦','1'=>'୧','2'=>'୨','3'=>'୩','4'=>'୪','5'=>'୫','6'=>'୬','7'=>'୭','8'=>'୮','9'=>'୯'];
+        $converted = strtr($time, $odiaDigits);
+        $converted = str_replace('AM', 'ଏ.ଏମ.', $converted);
+        $converted = str_replace('PM', 'ପି.ଏମ.', $converted);
+        return $converted;
+    }
 }
-@endphp
