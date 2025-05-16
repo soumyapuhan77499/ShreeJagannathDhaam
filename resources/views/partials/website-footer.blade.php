17283d4515020afc505e614966b0ec33c8cd9ffa
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-16 17:38:32 +0530

    d

--- resources/views/partials/website-footer.blade.php
+++ resources/views/partials/website-footer.blade.php
@@ -18,10 +18,18 @@
             <div>
                 <h3>{{ $language === 'Odia' ? 'ମନ୍ଦିର ସୂଚନା' : 'Temple Information' }}</h3>
                 <ul>
-                    <li><a href="{{ url('/online-donation') }}">{{ $language === 'Odia' ? 'ଅନଲାଇନ୍ ଦାନ' : 'Online Donation' }}</a></li>
-                    <li><a href="{{ url('/hundi-collection') }}">{{ $language === 'Odia' ? 'ହୁଣ୍ଡି ସଂଗ୍ରହ' : 'Hundi Collection' }}</a></li>
-                    <li><a href="{{ route('do-and-donts') }}">{{ $language === 'Odia' ? 'କରଣୀୟ ଓ ଅକରଣୀୟ' : "Do's and don'ts" }}</a></li>
-                    <li><a href="https://shreejagannathadham.com/puri-website/privacy-policy">{{ $language === 'Odia' ? 'ଗୋପନୀୟତା ନୀତି' : 'Privacy Policy' }}</a></li>
+                    <li><a
+                            href="{{ url('/online-donation') }}">{{ $language === 'Odia' ? 'ଅନଲାଇନ୍ ଦାନ' : 'Online Donation' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/hundi-collection') }}">{{ $language === 'Odia' ? 'ହୁଣ୍ଡି ସଂଗ୍ରହ' : 'Hundi Collection' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ route('do-and-donts') }}">{{ $language === 'Odia' ? 'କରଣୀୟ ଓ ଅକରଣୀୟ' : "Do's and don'ts" }}</a>
+                    </li>
+                    <li><a
+                            href="https://shreejagannathadham.com/puri-website/privacy-policy">{{ $language === 'Odia' ? 'ଗୋପନୀୟତା ନୀତି' : 'Privacy Policy' }}</a>
+                    </li>
                 </ul>
             </div>
 
@@ -30,9 +38,14 @@
                 <h3>{{ $language === 'Odia' ? 'ତ୍ଵରିତ ସେବା' : 'Quick Services' }}</h3>
                 <ul>
                     <li><a href="{{ url('/view-all-niti') }}">{{ $language === 'Odia' ? 'ନୀତି' : 'Niti' }}</a></li>
-                    <li><a href="{{ url('/darshan-timeline') }}">{{ $language === 'Odia' ? 'ଦର୍ଶନ' : 'Darshan' }}</a></li>
-                    <li><a href="{{ url('/maha-prasad') }}">{{ $language === 'Odia' ? 'ମହାପ୍ରସାଦ' : 'Mahaprasad' }}</a></li>
-                    <li><a href="{{ url('/view-festival-details') }}">{{ $language === 'Odia' ? 'ମନ୍ଦିର ପର୍ବ' : 'Temple Festival' }}</a></li>
+                    <li><a href="{{ url('/darshan-timeline') }}">{{ $language === 'Odia' ? 'ଦର୍ଶନ' : 'Darshan' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/maha-prasad') }}">{{ $language === 'Odia' ? 'ମହାପ୍ରସାଦ' : 'Mahaprasad' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/view-festival-details') }}">{{ $language === 'Odia' ? 'ମନ୍ଦିର ପର୍ବ' : 'Temple Festival' }}</a>
+                    </li>
                 </ul>
             </div>
 
@@ -40,22 +53,40 @@
             <div>
                 <h3>{{ $language === 'Odia' ? 'ଯାତ୍ରୀମାନଙ୍କ ଆବଶ୍ୟକତା' : 'Conveniences' }}</h3>
                 <ul>
-                    <li><a href="{{url('/bhaktanibas-list')}}">{{ $language === 'Odia' ? 'ଭକ୍ତ ନିବାସ' : 'Bhakta Nibas' }}</a></li>
-                    <li><a href="{{url('/parking-list')}}">{{ $language === 'Odia' ? 'ପାର୍କିଂ ସ୍ଥଳ' : 'Parking Areas' }}</a></li>
-                    <li><a href="{{url('/locker-shoe-list')}}">{{ $language === 'Odia' ? 'ଲକର ଓ ଜୁତା ସ୍ଥାନ' : 'Locker and Shoes Stands' }}</a></li>
-                    <li><a href="{{ url('/services/drinking_water') }}">{{ $language === 'Odia' ? 'ପାନୀୟ ଜଳ' : 'Drinking Water' }}</a></li>
-                    <li><a href="{{ url('/services-emergency') }}">{{ $language === 'Odia' ? 'ଜରୁରୀକାଳୀନ ଯୋଗାଯୋଗ' : 'Emergency' }}</a></li>
+                    <li><a
+                            href="{{ url('/bhaktanibas-list') }}">{{ $language === 'Odia' ? 'ଭକ୍ତ ନିବାସ' : 'Bhakta Nibas' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/parking-list') }}">{{ $language === 'Odia' ? 'ପାର୍କିଂ ସ୍ଥଳ' : 'Parking Areas' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/locker-shoe-list') }}">{{ $language === 'Odia' ? 'ଲକର ଓ ଜୁତା ସ୍ଥାନ' : 'Locker and Shoes Stands' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/services/drinking_water') }}">{{ $language === 'Odia' ? 'ପାନୀୟ ଜଳ' : 'Drinking Water' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/services-emergency') }}">{{ $language === 'Odia' ? 'ଜରୁରୀକାଳୀନ ଯୋଗାଯୋଗ' : 'Emergency' }}</a>
+                    </li>
                 </ul>
             </div>
 
             <!-- Extra Convenience Links -->
             <div>
                 <ul>
-                    <li><a href="{{ url('/services-abled') }}">{{ $language === 'Odia' ? 'ବିଶେଷ ସକ୍ଷମ ବ୍ୟକ୍ତି' : 'Special Abled Person' }}</a></li>
-                    <li><a href="{{ url('/services/lost_and_found_booth') }}">{{ $language === 'Odia' ? 'ହଜିବା ଓ ଖୋଜିବା କେନ୍ଦ୍ର' : 'Lost And Found' }}</a></li>
-                    <li><a href="{{ url('/services/toilet') }}">{{ $language === 'Odia' ? 'ଶୌଚାଳୟ' : 'Toilet' }}</a></li>
-                    <li><a href="{{ url('/services/beach') }}">{{ $language === 'Odia' ? 'ବେଳାଭୂମି' : 'Beaches' }}</a></li>
-                    <li><a href="{{ url('/services/life_guard_booth') }}">{{ $language === 'Odia' ? 'ଲାଇଫ ଗାର୍ଡଙ୍କ ଯୋଗାଯୋଗ' : 'Life Guards' }}</a></li>
+                    <li><a
+                            href="{{ url('/services-abled') }}">{{ $language === 'Odia' ? 'ବିଶେଷ ସକ୍ଷମ ବ୍ୟକ୍ତି' : 'Special Abled Person' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/services/lost_and_found_booth') }}">{{ $language === 'Odia' ? 'ହଜିବା ଓ ଖୋଜିବା କେନ୍ଦ୍ର' : 'Lost And Found' }}</a>
+                    </li>
+                    <li><a href="{{ url('/services/toilet') }}">{{ $language === 'Odia' ? 'ଶୌଚାଳୟ' : 'Toilet' }}</a>
+                    </li>
+                    <li><a href="{{ url('/services/beach') }}">{{ $language === 'Odia' ? 'ବେଳାଭୂମି' : 'Beaches' }}</a>
+                    </li>
+                    <li><a
+                            href="{{ url('/services/life_guard_booth') }}">{{ $language === 'Odia' ? 'ଲାଇଫ ଗାର୍ଡଙ୍କ ଯୋଗାଯୋଗ' : 'Life Guards' }}</a>
+                    </li>
                 </ul>
             </div>
         </div>
@@ -63,7 +94,7 @@
         <!-- App Download -->
         <div class="footer-bottom">
             <div class="footer-bottom-container">
-                <a href="https://play.google.com/store" target="_blank" class="app-button" style="background-color: #f8f8f8;">
+                <a href="https://play.google.com/store" target="_blank" class="app-button">
                     <img src="{{ asset('website/footer/google.webp') }}" alt="Google Play">
                 </a>
                 <a href="https://www.apple.com/app-store/" target="_blank" class="app-button">
@@ -71,13 +102,16 @@
                 </a>
             </div>
         </div>
+
     </div>
 </footer>
 
 <!-- Copyright -->
-<div style="height: 80px; width: 100%; background-color: #ffae35; display: flex; align-items: center; justify-content: center;">
+<div
+    style="height: 80px; width: 100%; background-color: #ffae35; display: flex; align-items: center; justify-content: center;">
     <div class="text-center text-sm text-black-400 mt-10 mb-6">
-        © {{ date('Y') }} {{ $language === 'Odia' ? 'ମନ୍ଦିର ପରିଚାଳନା ପ୍ରଣାଳୀ। ସମସ୍ତ ସ୍ୱତ୍ୱ ଅଧିକାର ସଂରକ୍ଷିତ।' : 'Temple Management System. All rights reserved.' }}
+        © {{ date('Y') }}
+        {{ $language === 'Odia' ? 'ମନ୍ଦିର ପରିଚାଳନା ପ୍ରଣାଳୀ। ସମସ୍ତ ସ୍ୱତ୍ୱ ଅଧିକାର ସଂରକ୍ଷିତ।' : 'Temple Management System. All rights reserved.' }}
     </div>
 </div>
 
