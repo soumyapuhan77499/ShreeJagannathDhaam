5a5a6df067edb1d29cad58d971e5538e87745ca2
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-17 11:55:47 +0530

    d

--- resources/views/website/index3.blade.php
+++ resources/views/website/index3.blade.php
@@ -179,7 +179,7 @@
                                     $formattedTime = \Carbon\Carbon::parse($startTime)->format('h:i A');
                                 @endphp
 
-                                {{ $language === 'Odia' ? convertToOdiaTime($formattedTime) : $formattedTime }}
+                                {{ $language === 'Odia' ? convertToOdiaTime($formattedTime) }}
                             @endif
                         </p>
 
