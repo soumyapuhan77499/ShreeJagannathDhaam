d1e5e8aa26a1d1bb4b4b230cce5951e0e221b205
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-19 16:07:24 +0530

    e

--- resources/views/website/bhaktanibas-list.blade.php
+++ resources/views/website/bhaktanibas-list.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-
+    
 <head>
     <meta charset="UTF-8">
     <title>Bhakta Niwas</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('front-assets/frontend/css/web-service.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('front-assets/frontend/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('front-assets/frontend/css/footer.css') }}">
