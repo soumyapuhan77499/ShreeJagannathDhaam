dfab8df38ef00ece7ed3468035aa7c95d7783e0e
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-19 17:23:12 +0530

    d

--- resources/views/partials/header-puri-dham.blade.php
+++ resources/views/partials/header-puri-dham.blade.php
@@ -2,14 +2,10 @@
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 <script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const toggles = document.querySelectorAll(".submenu-toggle");
-
-    toggles.forEach(toggle => {
-      toggle.addEventListener("click", () => {
-        const parent = toggle.closest(".has-submenu");
-        parent.classList.toggle("active");
-      });
+  document.querySelectorAll('.submenu-toggle').forEach(toggle => {
+    toggle.addEventListener('click', () => {
+      const parent = toggle.closest('.has-submenu');
+      parent.classList.toggle('active');
     });
   });
 </script>
