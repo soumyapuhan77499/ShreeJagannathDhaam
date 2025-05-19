ca06dbfe363b82503aa4b1eb3fb327484c15ecf4
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-19 11:52:28 +0530

    d

--- resources/views/website/index3.blade.php
+++ resources/views/website/index3.blade.php
@@ -689,7 +689,7 @@
                     <div class="convenience-item">
                         <img src="{{ asset('website/physical21.png') }}" alt="Special Abled">
                     </div>
-                    <p>{{ $language === 'Odia' ? 'ବିଶେଷ ସକ୍ଷମ ବ୍ୟକ୍ତି' : 'Special Abled Person' }}</p>
+                    <p class="mt-2 text-sm font-medium text-gray-800">{{ $language === 'Odia' ? 'ବିଶେଷ ସକ୍ଷମ ବ୍ୟକ୍ତି' : 'Special Abled Person' }}</p>
                 </a>
             </div>
 
@@ -699,7 +699,7 @@
                     <div class="convenience-item">
                         <img src="{{ asset('website/ph.png') }}" alt="Emergency">
                     </div>
-                    <p>{{ $language === 'Odia' ? 'ଜରୁରୀକାଳୀନ ଯୋଗାଯୋଗ' : 'Emergency' }}</p>
+                    <p class="mt-2 text-sm font-medium text-gray-800">{{ $language === 'Odia' ? 'ଜରୁରୀକାଳୀନ ଯୋଗାଯୋଗ' : 'Emergency' }}</p>
                 </a>
             </div>
 
@@ -709,7 +709,7 @@
                     <div class="convenience-item">
                         <img src="{{ asset('website/guard.png') }}" alt="Life Guards">
                     </div>
-                    <p>{{ $language === 'Odia' ? 'ଲାଇଫ ଗାର୍ଡଙ୍କ ଯୋଗାଯୋଗ' : 'Life Guards' }}</p>
+                    <p class="mt-2 text-sm font-medium text-gray-800">{{ $language === 'Odia' ? 'ଲାଇଫ ଗାର୍ଡଙ୍କ ଯୋଗାଯୋଗ' : 'Life Guards' }}</p>
                 </a>
             </div>
 
@@ -719,7 +719,7 @@
                     <div class="convenience-item">
                         <img src="{{ asset('website/lost.png') }}" alt="Lost and Found">
                     </div>
-                    <p>{{ $language === 'Odia' ? 'ହଜିବା ଓ ଖୋଜିବା କେନ୍ଦ୍ର' : 'Lost & Found' }}</p>
+                    <p class="mt-2 text-sm font-medium text-gray-800">{{ $language === 'Odia' ? 'ହଜିବା ଓ ଖୋଜିବା କେନ୍ଦ୍ର' : 'Lost & Found' }}</p>
                 </a>
             </div>
 
