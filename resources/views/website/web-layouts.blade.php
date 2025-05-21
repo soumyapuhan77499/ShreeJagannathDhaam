ec0146cbd72794d5405cca944fda671280bd6b26
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-05-21 16:38:06 +0530

    g

--- resources/views/website/web-layouts.blade.php
+++ resources/views/website/web-layouts.blade.php
@@ -285,7 +285,7 @@
         },
         breakpoints: {
             0: {
-                slidesPerView: 1.2,
+                slidesPerView: 2,
             },
             480: {
                 slidesPerView: 3,
@@ -322,7 +322,7 @@
         },
         breakpoints: {
             0: {
-                slidesPerView: 1.2,
+                slidesPerView: 2,
             },
             480: {
                 slidesPerView: 3,
