7d68c29873bad6d8b23049b56097d58bc3606591
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-06-06 16:09:35 +0530

    s

--- resources/views/website/parking-list.blade.php
+++ resources/views/website/parking-list.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="{{ asset('front-assets/frontend/css/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('front-assets/frontend/css/dham-header.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
     <style>
         .service-card img,
